bc4c277fa586cd0fd928ed6a7e0fdba3f81a3884
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2021-02-18 19:42:20 +0700

    AAA

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pyrobogui
 import time
 import random
 import webbrowser
@@ -7,26 +8,37 @@ from key import username, password
 googleUrl = 'https://www.google.com/?client=safari&channel=mac_bm'
 
 
-def waitForLoadPage(image, timeMin, timeMax):
+def waitForLoadPage(image, timeMin, timeMax, timeClick):
     while True:
         time.sleep(random.randint(timeMin, timeMax))
         logo = pyautogui.locateOnScreen(image)
         if logo is not None:
             x, y = pyautogui.locateCenterOnScreen(image)
-            pyautogui.click(x / 2, y / 2)
+            if timeClick > 1:
+                for i in range(timeClick):
+                    time.sleep(1)
+                    pyautogui.click(x / 2, y / 2)
+            else:
+                time.sleep(1)
+                pyautogui.click(x / 2, y / 2)
             break
 
 
-time.sleep(5)
-webbrowser.open(googleUrl)
-waitForLoadPage('btnDangNhap.png', 3, 5)
-time.sleep(3)
-pyautogui.typewrite(username, 0.1)
-pyautogui.typewrite(['\n'])
+def login():
+    time.sleep(5)
+    webbrowser.open(googleUrl)
+    waitForLoadPage('btnDangNhap.png', 3, 5, 1)
 
-time.sleep(3)
-pyautogui.typewrite(password, 0.15)
-pyautogui.typewrite(['\n'])
+    time.sleep(3)
+    pyautogui.typewrite(username, 0.1)
+    pyautogui.typewrite(['\n'])
 
-waitForLoadPage('btnDongY.png', 3, 5)
+    time.sleep(3)
+    pyautogui.typewrite(password, 0.15)
+    pyautogui.typewrite(['\n'])
 
+    waitForLoadPage('btnDongY.png', 3, 5, 6)
+
+
+def testRobogui():
+    pyrobogui.robo.waitImageToAppear()
